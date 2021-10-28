f9cf87907972d8bdcd84c436e463f9bcb3630a42
Katelyn Bunker <kerbunker@gmail.com>
2021-10-27 21:03:10 -0400

    basic html and start question array

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+// Array of question objects
+var questions = [
+    {
+        number: 1,
+        question: "Commonly used data types Do Not include:",
+        answer: "3. alerts",
+        optionOne: "1. strings",
+        optionTwo: "2. booleans",
+        optionThree: "3. alerts",
+        optionFour: "4. numbers"
+    },
+    {
+        number: 2,
+        question: "The condition in an if/else statement is enclosed with _____.",
+        answer: "3. parenthesis",
+        optionOne: "1. quotes",
+        optionTwo: "2. curly brackets",
+        optionThree: "3. parenthesis",
+        optionFour: "4. square brackets"
+    },
+    {
+        number: 1,
+        question: "Commonly used data types Do Not include:",
+        answer: "3. alerts",
+        optionOne: "1. strings",
+        optionTwo: "2. booleans",
+        optionThree: "3. alerts",
+        optionFour: "4. numbers"
+    },
+];
