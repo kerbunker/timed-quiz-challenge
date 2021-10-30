10c3471af167c2620dfe138a2712eb7eeef5955b
Katelyn Bunker <kerbunker@gmail.com>
2021-10-29 20:55:22 -0400

    add questions and end game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 var quizDiv = document.querySelector("#quiz-div");
+var ansDiv = document.querySelector("#answer-status");
+var prevAns = document.querySelector("#prev-answer");
+var endDiv = document.querySelector("#end-quiz");
 var quizQuest = document.querySelector("#quiz-question");
 var optionOne = document.querySelector("#option1");
 var optionTwo = document.querySelector("#option2");
@@ -28,15 +31,40 @@ var questions = [
         optionFour: "4. square brackets"
     },
     {
-        number: 1,
-        question: "Commonly used data types Do Not include:",
-        answer: "3. alerts",
-        optionOne: "1. strings",
-        optionTwo: "2. booleans",
-        optionThree: "3. alerts",
-        optionFour: "4. numbers"
+        number: 3,
+        question: "Arrays in javascript can be used to store ____.",
+        answer: "option4",
+        optionOne: "1. numbers and strings",
+        optionTwo: "2. other arrays",
+        optionThree: "3. booleans",
+        optionFour: "4. all of the above"
     },
+    {
+        number: 4,
+        question: "String values must be enclosed with _____ when being assigned to variables",
+        answer: "option3",
+        optionOne: "1. commas",
+        optionTwo: "2. curly brackets",
+        optionThree: "3. quotes",
+        optionFour: "4. parenthesis"
+    },
+    {
+        number: 5,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer: "option4",
+        optionOne: "1. JavaScript",
+        optionTwo: "2. terminal/bash",
+        optionThree: "3. for loops",
+        optionFour: "4. console.log"
+    }
 ];
+
+var endQuiz = function() {
+    quizDiv.style.display = "none";
+    endDiv.style.display = "block";
+    document.querySelector("#final-score").textContent = "Your final score is " // + endTime
+
+}
 
 var changeQuestion = function() {
     quizQuest.textContent = questions[currentQuestion].question;
@@ -44,7 +72,7 @@ var changeQuestion = function() {
     optionTwo.textContent = questions[currentQuestion].optionTwo;
     optionThree.textContent = questions[currentQuestion].optionThree;
     optionFour.textContent = questions[currentQuestion].optionFour;
-}
+};
 
 var answerQuestion = function(event) {
     var ansOption = event.target;
@@ -52,14 +80,21 @@ var answerQuestion = function(event) {
     var ansID = ansOption.getAttribute('id');
     var lastQuest = currentQuestion;
     currentQuestion++;
-    var ansStatus;
     if (ansID === questions[lastQuest].answer) {
-        ansStatus = "correct";
+        prevAns.textContent = "Correct!";
     } else {
-        ansStatus = "wrong";
+        prevAns.textContent = "Wrong!";
+        //timer - 10 seconds
     }
+    ansDiv.style.display = "block";
+    if (questions[currentQuestion]) {
+        changeQuestion();
+    } else {
+        endQuiz();
+    }
+    
 
-}
+};
 
 var startQuiz = function() {
     var startDiv = document.getElementById("start-info");
