1f8082fede1b12c7d4e72c5aaf5b1b6f40b1c540
Katelyn Bunker <kerbunker@gmail.com>
2021-11-03 21:03:40 -0400

    Add high scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,15 @@ var optionFour = document.querySelector("#option4");
 var currentQuestion = 0;
 var startButtonEl = document.querySelector("#start-button");
 var questOptions = document.querySelector("#quest-options");
+var timer = document.querySelector('#timer');
+var quizTimer;
+var timeLeft;
+var highScores = [];
+var highScoreEl = document.querySelector("#high-scores");
+
+var timeRemaining = 75;
+
+//var quizTimer = setTimeout(endQuiz, 10000);
 // Array of question objects
 var questions = [
     {
@@ -60,11 +69,37 @@ var questions = [
 ];
 
 var endQuiz = function() {
+    //console.log("endQuiz reached");
     quizDiv.style.display = "none";
     endDiv.style.display = "block";
-    document.querySelector("#final-score").textContent = "Your final score is " // + endTime
+    clearInterval(timeLeft);
+    document.querySelector("#final-score").textContent = "Your final score is " + timeRemaining;
+    
+    
+    document.querySelector("#submit").addEventListener("click", function() {
+        var userName = document.querySelector("input[name='user-name']").value;
+        //console.log(userName);
+        var newScore = {
+            name: userName,
+            score: timeRemaining
+        };
+        console.log(newScore);
+        highScores = highScores || [];
+        highScores.push(newScore);
+        highScores.sort(function(a, b){
+            let x = a.score;
+            let y = b.score;
+            if (x < y) {return -1;}
+            if (x > y) {return 1;}
+            return 0;
+        });
+        endDiv.style.display = "none";
+        highScoreEl.style.display = "block";
+        showHighScores();
 
-}
+    })
+
+};
 
 var changeQuestion = function() {
     quizQuest.textContent = questions[currentQuestion].question;
@@ -84,30 +119,82 @@ var answerQuestion = function(event) {
         prevAns.textContent = "Correct!";
     } else {
         prevAns.textContent = "Wrong!";
-        //timer - 10 seconds
+        timeRemaining = timeRemaining - 10;
+        timer.textContent = "Time: " + timeRemaining;
+        clearTimeout(quizTimer);
+        quizTimer = setTimeout(endQuiz, timeRemaining * 1000);
+
     }
     ansDiv.style.display = "block";
     if (questions[currentQuestion]) {
         changeQuestion();
     } else {
+        clearTimeout(quizTimer);
         endQuiz();
     }
     
 
 };
 
+var updateTimer = function() {
+    timeRemaining--;
+    timer.textContent = "Time: " + timeRemaining;
+};
+
 var startQuiz = function() {
     var startDiv = document.getElementById("start-info");
+    quizTimer = setTimeout(endQuiz, 75000);
     console.log(startDiv);
     startDiv.style.display = "none";
     quizDiv.style.display = "block";
-
+    timer.textContent = "Time: " + timeRemaining;
+    timeLeft = setInterval(updateTimer, 1000);
     changeQuestion();
-
-    
-     
 };
+
+var loadHighScores = function() {
+    highScores = localStorage.getItem("scores");
+
+    if (!highScores) {
+        return false;
+    }
+    highScores = JSON.parse(highScores);
+    
+};
+
+var showHighScores = function() {
+    if (highScores.length === 0) {
+        return false;
+    }
+    var scoreListEl = document.querySelector("#score-list");
+    for (var i = 0; i < highScores.length; i++) {
+        var scoreEl = document.createElement("li");
+        scoreEl.className = "score-item";
+        var scoreNameDiv = document.createElement("div");
+        var scoreNumDiv = document.createElement("div");
+        scoreEl.appendChild(scoreNameDiv);
+        scoreEl.appendChild(scoreNumDiv);
+        var scoreName = document.createElement("p");
+        scoreName.className = "score-name";
+        scoreName.textContent = highScores[i].name;
+
+        scoreNameDiv.appendChild(scoreName);
+
+        var scoreNum = document.createElement("p");
+        scoreNum.className = "score-num";
+        scoreNum.textContent = highScores[i].score;
+        
+        scoreNumDiv.appendChild(scoreNum);
+        scoreListEl.appendChild(scoreEl);
+
+    }
+}
 
 startButtonEl.addEventListener("click", startQuiz);
 questOptions.addEventListener("click", answerQuestion);
+loadHighScores();
 
+
+// update styling
+// add high scores
+// add timer for how long correct and wrong are on screen for?
